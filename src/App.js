a64a95b1125cb8983f98767ca67dd693851b6570
Marynryn <murmar95@gmail.com>
2024-07-09 12:47:32 +0300

    gfnnthy

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import Form from "./components/Form/Form";
 import styled from "styled-components";
 import { Toaster } from "react-hot-toast";
 const Container = styled.div`
-  padding: 30px 40px;
-  width: calc(100% - 80px);
+    padding: 30px 16px;  
+ 
+    @media (min-width: 480px){
+        width: 400px;
+        margin: 0 auto;
+        padding: 30px 40px;
+    }
+    @media (min-width: 768px){
+        width: 688px;
+    }
     @media (min-width: 1140px) {
         width: 1000px;
-        margin: 0 auto;
+       
     }
 `;
 function App() {
