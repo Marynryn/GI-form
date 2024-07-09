bd8776a3aaa3199725e60c53087a91a6875e1dee
Marynryn <murmar95@gmail.com>
2024-07-09 10:48:47 +0300

    phone pattern

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -9,7 +9,7 @@ export const validationSchema = yup.object().shape({
   zipCode: yup.string().required("Postal / Zip Code is required"),
   phone: yup
     .string()
-    .matches(/^(?:\(\d{3}\)\s?)?\d{3}-\d{4}$/, "Wrong Phone")
+    .matches(/^(?:\(\d{3}\)\s?\d{3}-\d{4}|\d{10})$/, "Wrong Phone")
     .required("Phone is required"),
   email: yup.string().email("Wrong Email").required("Email is required"),
 });
